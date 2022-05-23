fe8ab93e376517a7f3849b16662f06281dfa13c6
lejames <okejames55@gmail.com>
2022-05-23 17:46:55 +0300

    Restaurant details information card

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { getPlacesData } from './api';
 const App = () => {
   const [ places, setPlaces ] = useState([]);
   const [ coordinates, setCoordinates ] = useState({});
-  const [ bounds, setBounds ] = useState(null);
+  const [ bounds, setBounds ] = useState({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
@@ -35,7 +35,7 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
          <Grid item xs={12} md={4}>
-           <List />
+           <List places={places}/>
          </Grid>
          <Grid item xs={12} md={8}>
            <Map 
--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
 export const getPlacesData = async (sw, ne) => {
-    console.log('bounds-->', sw, ne)
     try {
         const { data: { data }} = await axios.get(URL, {
             params: {
