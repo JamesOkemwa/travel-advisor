56db9e4f9c9d625e16d97d3c6f65d1b1b1130fde
lejames <okejames55@gmail.com>
2022-05-29 16:29:58 +0300

    Searching places on the map and fetching related restaurants, hotels and attractions data

--- src/App.js
+++ src/App.js
@@ -30,12 +30,12 @@ const App = () => {
   }, [rating]);
 
   useEffect(() => {
-    setIsLoading(true);
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
 
-    if (bounds) {
       getPlacesData(type, bounds.sw, bounds.ne)
         .then((data) => {
-          setPlaces(data);
+          setPlaces(data?.filter((place) => place.name && place.num_reviews > 0 ));
           setFilteredPlaces([]);
           setIsLoading(false);
       })
@@ -46,7 +46,9 @@ const App = () => {
   return (
     <>
       <CssBaseline />
-      <Header />
+      <Header 
+        setCoordinates={setCoordinates} 
+      />
       <Grid container spacing={3} style={{ width: '100%' }}>
          <Grid item xs={12} md={4}>
            <List 
